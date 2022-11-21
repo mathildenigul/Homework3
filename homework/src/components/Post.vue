4c179f2a988739d1e5830e7bdbff380d9fcfffda
evalottalepp <evalepp@gmail.com>
2022-11-21 23:12:23 +0200

    Added Post component

---
+++ homework/src/components/Post.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script>
+export default {
+    name: "Post",
+    props: ['profilePicture', 'date', 'picture', 
+            'content', 'likes']
+}
+</script>
+
+<style></style>
