d697aa51d694910fb9353e790cc11f628ae8d44a
Úku Parts <uku.parts@gmail.com>
2022-12-12 22:16:47 +0200

    add login and protect home page

--- homework/frontend/src/router/index.js
+++ homework/frontend/src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import auth from "../auth";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+
+    beforeEnter: async(to, from, next) => {
+      let authResult = await auth.authenticated();
+      if (!authResult) {
+          next('/login')
+      } else {
+          next();
+      }
+  }
   },
   {
     path: "/signup",
